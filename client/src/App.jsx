bbd425d151e97fe40782043931557745747deef9
PratikHande101 <pratikhande30@gmail.com>
2023-11-28 00:45:52 +0530

    Home structure done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
-import Home from './pages/Home';
-import CreatePost from './pages/CreatePost';
+import { Home, CreatePost} from './pages';
 
 import { logo } from './assets';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
+      <header 
+        className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
         <Link to="/">
           <img src={logo} alt="logo" className='w-28 object-contain'/>
         </Link>

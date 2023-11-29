f675e1aa6edd9ac521442cfbc1be706f5414b7f3
PratikHande101 <pratikhande30@gmail.com>
2023-11-29 13:09:10 +0530

    Completed generateImg function and fetched images

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -9,14 +9,34 @@ const CreatePost = () => {
   const navigate = useNavigate();
   const [form, setForm] = useState({
     name: '',
-    prompt: '',
+    prompt: "",
     photo: ''
   });
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const generateImage = () => {
+  const generateImage = async () => {
+    if (form.prompt) {
+      try {
+        setGeneratingImg(true);
+        const response = await fetch(`http://localhost:8080/api/v1/dall-e`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({prompt: form.prompt}),
+        });
+        const data = await response.json();
 
+        setForm({...form, photo: `data:image/jpeg;base64,${data.photo}`});
+      } catch (error) {
+        alert(error);
+      } finally {
+        setGeneratingImg(false);
+      }
+    } else {
+      alert('Please enter a prompt');
+    } 
   }
 
   const handleSubmit = () => {
--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -18,20 +18,23 @@ router.route('/').get((req, res) => {
 
 router.route('/').post(async (req, res) => {
     try {
-        const { prompt } = req.body;
-        const aiResponse = await openai.createImage({
+        const {prompt} = req.body;
+        console.log(prompt);
+        const aiResponse = await openai.images.generate({
             prompt: prompt,
-            n: `1`,
-            size: `1024*1024`,
+            n: 1,
+            size: `1024x1024`,
             response_format: `b64_json`,
         });
+        console.log(aiResponse);
 
-        const image = aiResponse.data.data[0].b64_json;
+        const image = aiResponse.data[0].b64_json;
 
         res.status(200).json({photo: image});
     } catch (error) {
         console.log(error);
-        res.status(500).send(error?.response.data.error.message);
+        //res.status(500).send(error?.response.data.error.message);
+        res.status(500).send(error);
     }
 });
 
